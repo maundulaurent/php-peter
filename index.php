1f0024b71f28437d79bab379483b5d2066d3ae6a
CHARLES MAUNDU <charlesmaundu16@gmail.com>
2024-10-24 12:04:25 +0300

    added a manifest

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ if (!isset($_SESSION['user_id'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Dashboard | Admin POS</title>
 
+  <link rel="manifest" href="manifest.json">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
